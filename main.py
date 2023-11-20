4e24e00c7b865b40950d58fa377737361a7b0cdb
николай <nicklaskar2008@gmail.com>
2023-11-20 17:12:38 +0300

    посмотрите этот код

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import sys
-from random import randint
 
-from PyQt5.QtGui import QColor
+
+from PyQt5.QtGui import QColor, QPainter
 from PyQt5.QtWidgets import QWidget, QApplication, QPushButton
 
 
@@ -13,15 +13,15 @@ class Suprematism(QWidget):
         self.push_btn.move(50, 50)
         self.push_btn.resize(200, 100)
         self.push_btn.clicked.connect(self.draw)
-
-    def draw(self):
-        R = randint(20, 100)
-        self.qp.setBrush(QColor(255, 255, 0))
-        self.qp.drawEllipse()
+        self.qp = QPainter()
 
     def initUI(self):
         self.setGeometry(300, 300, 1000, 1000)
         self.setWindowTitle('Рисование')
+
+    def draw(self):
+        self.qp.setBrush(QColor(255, 255, 0))
+        self.qp.drawEllipse((100, 100, 150, 200), fill='yellow', outline=(0, 0, 0))
 
 
 if __name__ == '__main__':
