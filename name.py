820fb2182ed0cf0408c0fb9c6999025d1ab2cb6d
Николай Ласкаржевский <nicklaskar2008@gmail.com>
2023-11-22 23:20:22 +0300

    заданеи 2 на репозиторий домашняя рабjnf

--- name.py
+++ name.py
@@ -1,28 +1,76 @@
 import sqlite3
 import sys
-from PyQt5 import uic, Qt
-from main_ui import Ui_MainWindow
+from PyQt5 import uic
+from addEditCoffeeForm_ui import Ui_MainWindow
 from PyQt5.QtWidgets import QApplication, QMainWindow, QTableWidgetItem
 
 
 class Test(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
-        uic.loadUi('main.ui', self)
+        uic.loadUi('addEditCoffeeForm.ui', self)
         self.con = sqlite3.connect('coffe.sqlite.db')
         self.cur = self.con.cursor()
+        it = [i[0] for i in self.cur.execute("SELECT id FROM coffes").fetchall()]
+        for i in it:
+            self.comboBox_2.addItem(str(i))
+        self.pushButton.clicked.connect(self.change)
+        self.pushButton_2.clicked.connect(self.add)
         self.table()
 
     def table(self):
-        cur = self.con.cursor()
-        result = cur.execute("""select name_sort, stepen_fire, vid_coffe, opicane, cost, obem from coffes""").fetchall()
+        result = self.cur.execute(
+            """select id, name_sort, stepen_fire, vid_coffe, opicane, cost, obem from coffes""").fetchall()
         self.tableWidget.setRowCount(len(result))
         self.tableWidget.setColumnCount(len(result[0]))
         self.tableWidget.setHorizontalHeaderLabels(
-            ["Название сорта", "Степень прожарки", "Вид коффе", "Описание", "Цена", "Объем"])
+            ["id", "Название сорта", "Степень прожарки", "Вид коффе", "Описание", "Цена", "Объем"])
         for i, elem in enumerate(result):
             for j, val in enumerate(elem):
                 self.tableWidget.setItem(i, j, QTableWidgetItem(str(val)))
+
+    def change(self):
+        if self.comboBox.currentText() == 'name_sort':
+            que = self.cur.execute(f"""UPDATE coffes
+                                       SET name_sort = '{self.lineEdit.displayText()}'
+                                       WHERE id = '{self.comboBox_2.currentIndex() + 1}'""")
+            self.con.commit()
+
+        elif self.comboBox.currentText() == 'stepen_fire':
+            que = self.cur.execute(f"""UPDATE coffes
+                                       SET stepen_fire = '{self.lineEdit.displayText()}'
+                                       WHERE id = '{self.comboBox_2.currentIndex() + 1}'""")
+            self.con.commit()
+
+        elif self.comboBox.currentText() == 'vid_coffe':
+            que = self.cur.execute(f"""UPDATE coffes
+                                       SET vid_coffe = '{self.lineEdit.displayText()}'
+                                       WHERE id = '{self.comboBox_2.currentIndex() + 1}'""")
+            self.con.commit()
+
+        elif self.comboBox.currentText() == 'opicane':
+            que = self.cur.execute(f"""UPDATE coffes
+                                       SET opicane = '{self.lineEdit.displayText()}'
+                                       WHERE id = '{self.comboBox_2.currentIndex() + 1}'""")
+            self.con.commit()
+
+        elif self.comboBox.currentText() == 'cost':
+            que = self.cur.execute(f"""UPDATE coffes
+                                       SET cost = '{self.lineEdit.displayText()}'
+                                       WHERE id = '{self.comboBox_2.currentIndex() + 1}'""")
+            self.con.commit()
+
+        elif self.comboBox.currentText() == 'obem':
+            que = self.cur.execute(f"""UPDATE coffes
+                                       SET obem = '{self.lineEdit.displayText()}'
+                                       WHERE id = '{self.comboBox_2.currentIndex() + 1}'""")
+            self.con.commit()
+        self.table()
+
+    def add(self):
+        que = self.cur.execute(f"""INSERT INTO coffes(name_sort) VALUES('{self.lineEdit_2.displayText()}')""")
+        self.con.commit()
+        self.table()
 
 
 if __name__ == '__main__':
